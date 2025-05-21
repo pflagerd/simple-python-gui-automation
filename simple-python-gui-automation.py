d97dc8f270a74a40eb89c2798ac9093120f08729
Daniel P Pflager <daniel.pflager@gmail.com>
2025-05-20 21:11:20 -0700

    progress

---
+++ simple-python-gui-automation.py
@@ -0,0 +1,22 @@
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pytest
+import time
+
+chromedriver_autoinstaller.install()
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+def test_google_search(driver):
+    driver.get("https://www.google.com")
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys("Selenium WebDriver" + Keys.RETURN)
+    print("driver.title == " + driver.title)
+    time.sleep(10)
+    # assert "Selenium WebDriver" in driver.title
